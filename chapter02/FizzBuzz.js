f3ed514938ef8c3193af885fc00f35a54f5bddcc
Chuma Mcphoy <chmcphy@gmail.com>
2016-08-03 03:10:16 +0200

    Create FizzBuzz.js

---
+++ chapter02/FizzBuzz.js
@@ -0,0 +1,22 @@
+// My Solution
+for (var n = 1; n < 100; n++) {
+  if (n % 3 == 0 && n % 5 == 0)
+    console.log("FizzBuzz")
+  else if (n % 3 == 0)
+    console.log("Fizz");
+  else if (n % 5 == 0)
+    console.log("Buzz");
+  else
+    console.log(n)
+}
+
+// Martin's Solution
+for (var n = 1; n <= 100; n++) {
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
+}
+
